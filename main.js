91e36d5e70b28b95112fea26dc490f38e5c166ca
xeecos <flashindream@gmail.com>
2016-11-19 18:08:44 +0800

    update

--- main.js
+++ main.js
@@ -2,63 +2,34 @@ const fs = require('fs');
 const https = require('https');
 const querystring = require('querystring');
 const guid = require('guid');
-var mic = require('mic');
-var wav = require('node-wav');
+var Mic = require('node-microphone');
+var mic = new Mic({bitwidth:16,rate:8000,channels:1});
+var recordVoiceStream = fs.WriteStream("./output.wav");
 
-var wavBin;// = fs.readFileSync('./assets/test.wav');
+var lastTime = new Date().getTime();
 
-var micInstance = mic({ 'rate': '8000', 'channels': '1', 'debug': true, 'exitOnSilence': 6 });
-var micInputStream = micInstance.getAudioStream();
+startRecording(3000);
 
-var outputWavStream = fs.WriteStream('output.wav');
-micInputStream.pipe(outputWavStream);
- 
-micInputStream.on('data', function(data) {
-    console.log("Recieved Input Stream: " + data.length);
+mic.on('info', (info) => {
+// 	console.log("info:",info);
 });
- 
-micInputStream.on('error', function(err) {
-    cosole.log("Error in Input Stream: " + err);
+mic.on('error', (error) => {
+// 	console.log("error:",error);
 });
- 
-micInputStream.on('startComplete', function() {
-        console.log("Got SIGNAL startComplete");
-        setTimeout(function() {
-                micInstance.pause();
-            }, 5000);
-    });
-    
-micInputStream.on('stopComplete', function() {
-        console.log("Got SIGNAL stopComplete");
-	wavBin = wav.encode(outputWavStream, { sampleRate: 8000, float: true, bitDepth: 32 });
-    });
-    
-micInputStream.on('pauseComplete', function() {
-        console.log("Got SIGNAL pauseComplete");
-        setTimeout(function() {
-                micInstance.resume();
-            }, 5000);
-    });
- 
-micInputStream.on('resumeComplete', function() {
-        console.log("Got SIGNAL resumeComplete");
-        setTimeout(function() {
-                micInstance.stop();
-            }, 5000);
-    });
- 
-micInputStream.on('silence', function() {
-        console.log("Got SIGNAL silence");
-    });
- 
-micInputStream.on('processExitComplete', function() {
-        console.log("Got SIGNAL processExitComplete");
-    });
- 
-micInstance.start();
-//requestAuth('1267e760dd2748aa9165ae885e7d5729');
 
+function startRecording(time){
+	var micStream = mic.startRecording();
+	micStream.pipe(recordVoiceStream);
+	setTimeout(stopRecording,time);
+}
+
+function stopRecording(){
+	lastTime = new Date().getTime();
+    mic.stopRecording();
+    requestAuth('1267e760dd2748aa9165ae885e7d5729');
+}
 function requestAuth(speechKey){
+	console.log("requestAuth");
 	var post_options = {
 		method: 'POST',
 		host: 'api.cognitive.microsoft.com',
@@ -70,7 +41,7 @@ function requestAuth(speechKey){
 		}
 	};
 	
-	var post_req = https.request(post_options, (res) => {
+	var authReq = https.request(post_options, (res) => {
 	    var chunks = '';
 	    res.setEncoding('utf8');
 	    res.on('data', (chunk) => {
@@ -78,13 +49,17 @@ function requestAuth(speechKey){
 	    });
 	    res.on('end', () => {
 	        var error, result;
-		requestSpeech(chunks);
+			requestSpeech(chunks);
 	    });
 	});
-	post_req.write("");
-	post_req.end();
+	authReq.on('error', (e) => {
+	  console.error(e);
+	});
+	authReq.write("");
+	authReq.end();
 }
 function requestSpeech(authCode){
+	console.log("requestSpeech");
 	var post_params = {
 	    'version': '3.0',
 	    'requestid': guid.raw(), // changes each call
@@ -112,12 +87,18 @@ function requestSpeech(authCode){
 	    });
 	    res.on('end', () => {
 	        var error, result;
-		try {
-			result = JSON.parse(chunks);
-			console.log(result.header.name);
-		} catch (e) {
+			try {
+				result = JSON.parse(chunks);
+				console.log("result:",result.header.name);
+			} catch (e) {
       		}
+      		
+			console.log("time:"+Math.round((new Date().getTime()-lastTime)/1000));
 	    });
+	});
+	var wavBin = fs.readFileSync("./output.wav");
+	speechReq.on('error', (e) => {
+	  console.error(e);
 	});
 	speechReq.write(wavBin);
 	speechReq.end();
