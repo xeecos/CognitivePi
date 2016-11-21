4a07297a22e66223f9e7f7caf1ee165216e85061
xeecos <flashindream@gmail.com>
2016-11-21 21:32:23 +0800

    update

--- demo.js
+++ demo.js
@@ -3,23 +3,25 @@ var mic = require('microphone');
 mic.startCapture();
 
 mic.audioStream.on('data', function(data) {
-	var l = getRMS(data);
-	if(l>1.36){
+    var l = getRMS(data);
+    if(l>0.02){
     	console.log(l); 
     }
 });
 
 var SHORT_NORMALIZE = (1.0/32768.0)
 function getRMS(buffer){
-	var count = buffer.length/2;
+    var count = buffer.length/2;
     var sum_squares = 0.0;
     var shorts = []
     for(var i=0;i<count;i++){
-    	shorts.push(buffer.readUInt16LE(i*2));
-	}
+    	shorts.push(buffer.readInt16LE(i*2));
+    }
     for (var i=0;i<count;i++){
         var n = shorts[i] * SHORT_NORMALIZE
         sum_squares += n*n
-	}
+    }
     return Math.sqrt( sum_squares / count )
 }
+
+// : spawn('arecord', ['-D', 'plughw:1,0', '-f', 'S16_LE','-r','8000']);
