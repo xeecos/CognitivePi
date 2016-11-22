97e1eebf3095c2050ae3aabcbb81522ae17f68e6
xeecos <flashindream@gmail.com>
2016-11-22 12:19:53 +0800

    update

--- demo.js
+++ demo.js
@@ -1,12 +1,39 @@
 var mic = require('microphone');
-
+const fs = require("fs");
+const WavEncoder = require("wav-encoder");
+var list = []; 
+var header;
 mic.startCapture();
 
+setTimeout(function(){
+	mic.stopCapture();
+	var count = list.length;
+	header.writeUInt32LE(count*list[0].length,4);
+	var buffer = new Buffer(count*list[0].length+44);
+	for(var i=0;i<44;i++){
+		buffer[i] = header[i];
+	}
+	for(var i=0;i<count;i++){
+		var len = list[i].length;	
+		for(var j=0;j<len;j++){
+			index = 44+len*i+j;
+			buffer[index] = list[i][j];
+		}
+	}
+	fs.writeFileSync("noise.wav", new Buffer(buffer));
+},4000);
 mic.audioStream.on('data', function(data) {
-    var l = getRMS(data);
+	if(data.length>44)
+		list.push(data);
+	else{
+		header = data;
+		console.log(data.readUInt16LE(22));
+		console.log(data.readUInt32LE(24));
+	}
+    /*var l = getRMS(data);
     if(l>0.02){
     	console.log(l); 
-    }
+    }*/
 });
 
 var SHORT_NORMALIZE = (1.0/32768.0)
